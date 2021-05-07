24aedb11ed51edc1ce68c6cc89d26d360b80c856
anagha778 <anagha778@gmail.com>
2021-05-06 20:49:31 -0400

    remove userproductReducer

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -20,7 +20,7 @@ import {
     cartOpen: false
   };
   
-  export const reducer = (state = initialState, action) => {
+  export const reducers = (state = initialState, action) => {
     switch (action.type) {
       // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
       case UPDATE_PRODUCTS:
@@ -95,6 +95,8 @@ import {
     }
   };
 
+  /* remove useProductReducer function
   export function useProductReducer(initialState) {
     return useReducer(reducer, initialState);
   }
+  */
